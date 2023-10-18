e0f584010ed4e59f35d3f35275b1cd16635e0c82
OvidiuPlesa <plesaovidiu86@gmail.com>
2023-10-18 17:56:36 +0300

    Tema1

---
+++ src/com/itFactory/Exercitiu1.java
@@ -0,0 +1,9 @@
+package com.itFactory;
+
+public class Exercitiu1 {
+
+    public static void main(String[] args) {
+        System.out.println("primul meu program");
+    }
+
+}
---
+++ src/com/itFactory/HelloWorld.java
@@ -0,0 +1,29 @@
+package com.itFactory;
+
+public class HelloWorld {
+
+    // public -> este un modificator de acces. Face ca metoda sa fie vizibila in tot proiectul. (Comentariu pe o singura linie)
+    /*
+    * static - nu avem nevoie de instanta/obiect pentru a apela aceasta metoda
+    * void -> inseamna ca aceasta metoda nu returneaza nimic
+    * ----> acesta este un comentariu pe mai multe linii
+    *
+    * */
+
+
+    /**
+     * main -> aceasa este metoda principala care porneste programul
+     * fara aceasta metoda nu vom putea porni programul
+     *  String[] args -> un array de tip String prezinta argumentele programului
+     *
+     */
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+
+
+    }
+
+}
