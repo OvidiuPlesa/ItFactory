39dede8864e1b195586028c1cf2c342fc6135ba4
OvidiuPlesa <130586016+OvidiuPlesa@users.noreply.github.com>
2023-11-06 18:22:45 +0200

    Add files via upload

---
+++ Sapt3_Curs5_6_Tema_OvidiuPlesa/src/com/itFactory/problema1/Main.java
@@ -0,0 +1,13 @@
+package com.itFactory.problema1;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+
+
+
+    }
+}
